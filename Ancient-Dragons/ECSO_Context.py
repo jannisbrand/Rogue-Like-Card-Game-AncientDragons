4f5994f3a4941dcfbfe8d2ef476854491a06c7ab
Melina Schübbe <melina.schuebbe@weidmueller.com>
2025-02-27 08:52:58 +0100

    card_system created

--- Ancient-Dragons/ECSO_Context.py
+++ Ancient-Dragons/ECSO_Context.py
@@ -4,6 +4,7 @@ import pygame
 
 from Characters import Base
 from Sprites.Base import Sprite
+from Components import Components
 
 """
 TODO:
@@ -183,3 +184,245 @@ class ECSO_Context():
                     self.objects[type].pop(index)
         except KeyError as e:
             print("", e)
+
+    def attack_modifiers(self, value_to_modify, list_of_components) -> int:
+        for comp in list_of_components:
+            match comp:
+                case Components.C_ATTACK_PLUS:
+                    value_to_modify + comp.value
+                    pass
+                case Components.C_ATTACK_ALL:
+                    # We only have one enemy currently
+                    pass
+                case Components.C_ATTACK_RANDOM:
+                    # still only one enemy. It will target the one enemy
+                    pass
+                case Components.C_ATTACK_NUM:
+                    # the value of the component is the number of seperate attacks
+                    pass
+                case Components.C_ATTACK_INCREASE:
+                    value_to_modify + comp.value
+                    # attack is increased by the cards played
+                    # need to check the played cards
+                    pass
+                case Components.C_ATTACK_MULT:
+                    value_to_modify * comp.value
+                    pass
+                case Components.C_EXHAUST_ATK:
+                    # for every exhausted card gain extra atk
+                    # need to get the number of exhausted cards before
+                    value_to_modify + comp.value 
+                    pass
+        return value_to_modify
+            
+    def card_system(self, selected_card: int, selected_target: int) -> bool:
+        components = self.get_components(selected_card)
+        target = self.get_object("CHARACTERS", selected_target)
+        
+        for component in components:
+            match type(component):
+                case Components.C_ATTACK:
+                    if target.type == "ENEMY":
+                        # seperate funktion für die veränderung von attack
+                        attack = self.attack_modifiers(component.value, components) - target.effect["BLOCK"]
+                        target.health_points -= attack
+                        pass
+                case Components.C_DEFENSE:
+                    if target.type == "CHARATER":
+                        # def is missing for the charakter
+                        pass
+                case Components.C_DEFENSE_STAY:
+                    # still no def i could reference
+                    pass
+                case Components.C_EXHAUST_PLAYED:
+                    # Exhaust every card played from the value typ
+                    pass
+                case Components.C_EXHAUST_TO_HAND_CHOOSE:
+                    # choose one exhausted card and add it to the hand
+                    # need access to the exhaust pile and hand
+                    pass
+                case Components.C_EXHAUST_TO_HAND:
+                    # add this exhausted card to the hand
+                    # check the current played card and add to hand
+                    pass
+                case Components.C_EXHAUST_DEF:
+                    # for ervery exhausted card gain value def
+                    # get number exhausted cards mult value with number cards
+                    pass
+                case Components.C_EXHAUST_HAND_NOATTACK:
+                    # exhaust all non attack type cards in the hand
+                    # check the hand for the no attack cards
+                    pass
+                case Components.C_EXHAUST_HAND_ALL:
+                    # exhaust all hand cards
+                    pass
+                case Components.C_EXHAUST_HAND_RANDOM:
+                    # exhaust one card random in the hand
+                    pass
+                case Components.C_EXHAUST_HAND:
+                    # choose one hand card to exhaust
+                    pass
+                case Components.C_EXHAUST:
+                    # add card to the exhaust pile
+                    pass
+                case Components.C_DAMAGE_DEF:
+                    if target.type == "ENEMY":
+                        # get current def
+                        # attack = self. - target.effect["BLOCK"]
+                        # target.health_points -= attack
+                        pass
+                    pass
+                case Components.C_DAMAGE:
+                    # player gets damage for the value, but block can reduce it
+                    pass
+                case Components.C_COPY_SAME_DP:
+                    # copy this card and add it to the discard pile
+                    pass
+                case Components.C_COPY_TO_HAND:
+                    # copy a card to the hand
+                    # will be removed after the combat round
+                    pass
+                case Components.C_DRAW_RANDOM_ATTACK:
+                    # get random attack type card and let it cost the value
+                    pass
+                case Components.C_DRAW:
+                    # draw value number of cards
+                    pass
+                case Components.C_GAIN_HP:
+                    if target.type == "CHARATER":
+                        target.health_points += component.value
+                    pass
+                case Components.C_GAIN_MANA:
+                    if target.type == "CHARATER":
+                        target.mana_points += component.value
+                    pass
+                case Components.C_LOSE_HP:
+                    if target.type == "CHARATER":
+                        target.health_points -= component.value
+                    pass
+                case Components.C_LOSE_MANA:
+                    if target.type == "CHARATER":
+                        target.mana_points -= component.value
+                    pass
+                case Components.C_ETHEREAL:
+                    # card not played at the end of the round add it to exhaust pile
+                    pass
+                case Components.C_UNPLAYABLE:
+                    # card not playable
+                    pass
+                case Components.C_INNATE:
+                    # first combat round has this card in the hand
+                    pass
+                case Components.C_ROUND:
+                    # the next effect happends in the next round
+                    pass
+                case Components.C_END_TURN:
+                    # the next effect happends at the end of the turn
+                    pass
+                case Components.C_NO_DRAW:
+                    # player can't draw more cards this turn
+                    # could be a status effect
+                    pass
+                case Components.C_CARD_LIMIT:
+                    # only value number of cards can be played
+                    pass
+                case Components.C_UPGARDE_COMBAT:
+                    # upgarde a card for the current fight only
+                    pass
+                case Components.C_UPGARDE_NOLIMIT:
+                    # card can be upgraded multiply times
+                    pass
+                case Components.C_UPGRADE:
+                    # upgared a card
+                    pass
+                case Components.C_SKILL_FREE:
+                    # card type skill costs zero this turn
+                    pass
+                case Components.C_NEXT_ATTACK_TWICE:
+                    # next attack card will be played twice
+                    pass
+                case Components.C_DP_DWP:
+                    # put a number of cards from the discard pile to the draw pile. 
+                    # player chooses the cards
+                    #get the discard pile and add them to draw pile
+                    pass
+                case Components.C_ADD_DWP:
+                    # add a (value) number of specific (name) status cards to the draw pile
+                    pass
+                case Components.C_PLAY_DWP_EX:
+                    # play value number of cards from drawpile and add them to exhaust pile
+                    pass
+                case Components.C_COST_RED:
+                    # cost to play the card will be reduced (value each time) by the number of attacked the player got in this combat
+                    pass
+                case Components.C_HANDCARD_NUMBER_DAMAGE:
+                    # get number of hand cards
+                    # lose value number of HP per card
+                    pass
+                case Components.C_DEBUFF_ALL:
+                    if target.type == "ENEMY":
+                        # add buff/debuff to all enemy. value = buff name. round = number of rounds it stays
+                        pass
+                case Components.C_DEBUFF:
+                    if target.type == "ENEMY":
+                         # add buff/debuff to enemy. value = buff name. round = number of rounds it stays
+                        pass
+                case Components.C_BUFF:
+                    if target.type == "CHARATER"
+                        # add buff/debuff to the player. value = buff name. round = number of rounds it stays
+                        pass
+                case Components.C_WHEN_CURSE_OR_STATUS:
+                    # When the drawn card is a status or curse card
+                    pass
+                case Components.C_WHEN_CURSE:
+                    # when the drawn card is a curse card
+                    pass
+                case Components.C_WHEN_STATUS:
+                    # when the drawn card is a status card
+                    pass
+                case Components.C_WHEN_EXHAUST_DRAW:
+                    # when an exhausted card gets drawn. Draw a number of cards
+                    pass
+                case Components.C_WHEN_EXHAUST_CARD:
+                    # when a card gets exhausted. Do the next effects
+                    pass
+                case Components.C_WHEN_EXHAUST:
+                    # when this card gets exhausted
+                    pass
+                case Components.C_WHEN_BUFF:
+                    # when the player has the (value) named effect
+                    pass
+                case Components.C_WHEN_DEBUFF:
+                    # when the enemy has the (value) named effect
+                    pass
+                case Components.C_WHEN_ONLY:
+                    # when the hand cards only have cards from the (value) named typ in it
+                    pass
+                case Components.C_WHEN_ATTACKED:
+                    # when the player gets attacked in this turn
+                    pass
+                case Components.C_WHEN_NOT_PLAYED:
+                    # when this card is not played
+                    pass
+                case Components.C_WHEN_DRAWN:
+                    # when this card is drawn
+                    pass
+                case Components.C_WHEN_OTHER_CARDPLAYED:
+                    # when other cards are played
+                    pass
+                case Components.C_WHEN_REMOVED:
+                    # when the card is removed from the deck
+                    pass
+                case Components.C_WHEN_TYPE:
+                    # when cards from the (value) named typ are played
+                    pass
+                case Components.C_WHEN_INCOMING:
+                    # when the enemy will attack next round
+                    pass
+                case Components.C_WHEN_LOSE_HP_CARD:
+                    # when the player loses HP by a card effect
+                    pass
+                case Components.C_WHEN_DEF:
+                    # when the def of the player gets raised
+                    pass
+
